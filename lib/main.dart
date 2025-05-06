bf98be27f64883e61e6a093d0c9f31a5b661385f
Sandhya <sandhya.k@agilophiles.com>
2025-05-06 15:17:35 +0530

    Responsive code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,204 @@
+// import 'package:flutter/material.dart';
+// import 'package:newconnection_buildingpemitdetails/responsive_row_column.dart';
+//
+// void main() {
+//   runApp(
+//     MaterialApp(
+//       home: BuildingPermitDetailsPage(),
+//       debugShowCheckedModeBanner: false,
+//     ),
+//   );
+// }
+//
+// class BuildingPermitDetailsPage extends StatelessWidget {
+//   const BuildingPermitDetailsPage({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return LayoutBuilder(
+//       builder: (context, constraints) {
+//         return Scaffold(
+//           appBar: AppBar(
+//             title: Text("New Connection Form"),
+//             backgroundColor: Colors.blueGrey,
+//           ),
+//           body: SingleChildScrollView(
+//             padding: EdgeInsets.all(16),
+//             child: Container(
+//               padding: EdgeInsets.all(16),
+//               decoration: BoxDecoration(
+//                 color: Colors.white,
+//                 border: Border.all(color: Colors.grey.shade300),
+//                 borderRadius: BorderRadius.circular(16),
+//                 boxShadow: [
+//                   BoxShadow(
+//                     color: Colors.grey.shade200,
+//                     blurRadius: 8,
+//                     offset: Offset(0, 4),
+//                   ),
+//                 ],
+//               ),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   // Customer Name
+//                   ResponsiveRowColumn(
+//                     spacing: 10,
+//                     children: [
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Text("Building Permit Number *"),
+//                           SizedBox(
+//                             width: 450,
+//                             child: TextField(
+//                               decoration: InputDecoration(
+//                                 hintText: 'Enter Permit Name',
+//                                 border: OutlineInputBorder(),
+//                               ),
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                       // SizedBox(height: 1),
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Text("Building Area *"),
+//                           SizedBox(
+//                             width: 450,
+//                             child: TextField(
+//                               decoration: InputDecoration(
+//                                 hintText: 'Enter Building Area',
+//                                 border: OutlineInputBorder(),
+//                               ),
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                   SizedBox(height: 16,),
+//                   ResponsiveRowColumn(
+//                     spacing: 10,
+//                     children: [
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Text("Customer Class *"),
+//                           SizedBox(
+//                             width: 450,
+//                             child: TextField(
+//                               decoration: InputDecoration(
+//                                 hintText: 'Select Customer Class',
+//                                 border: OutlineInputBorder(),
+//                               ),
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                       // SizedBox(height: 1),
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Text("Premise Type *"),
+//                           SizedBox(
+//                             width: 450,
+//                             child: TextField(
+//                               decoration: InputDecoration(
+//                                 hintText: 'Select Premise Type',
+//                                 border: OutlineInputBorder(),
+//                               ),
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                     ],
+//                   ),
+//                   SizedBox(height: 16,),
+//                   ResponsiveRowColumn(
+//                     spacing: 10,
+//                     children: [
+//                       Column(
+//                         crossAxisAlignment: CrossAxisAlignment.start,
+//                         children: [
+//                           Text("Service Type"),
+//                           SizedBox(
+//                             width: 450,
+//                             child: TextField(
+//                               decoration: InputDecoration(
+//                                 hintText: 'Select Service Type',
+//                                 border: OutlineInputBorder(),
+//                               ),
+//                             ),
+//                           ),
+//                         ],
+//                       ),
+//                       // SizedBox(height: 1),
+//                       // Container()
+//                       // SizedBox(width: 250), // fixed Expanded issue
+//                     ],
+//                   ),
+//
+//                   SizedBox(height: 32),
+//                   Center(
+//                     child: Row(
+//                       mainAxisAlignment: MainAxisAlignment.center,
+//                       children: [
+//                         SizedBox(
+//                           width: 110,
+//                           height: 38,
+//                           child: ElevatedButton(
+//                             onPressed: () {
+//                               ScaffoldMessenger.of(context).showSnackBar(
+//                                 SnackBar(content: Text('Back pressed')),
+//                               );
+//                             },
+//                             style: ElevatedButton.styleFrom(
+//                               backgroundColor: Colors.white,
+//                               side: BorderSide(color: Colors.redAccent, width: 1), // 🔴 red border
+//                             ),
+//                             child: Text(
+//                               'Back',
+//                               style: TextStyle(color: Colors.redAccent),
+//                             ),
+//                           ),
+//                         ),
+//                         SizedBox(width: 16),
+//                         SizedBox(
+//                           width: 110,
+//                           height: 38,
+//                           child: ElevatedButton(
+//                             onPressed: () {
+//                               ScaffoldMessenger.of(context).showSnackBar(
+//                                 SnackBar(content: Text('Continue pressed')),
+//                               );
+//                             },
+//                             style: ElevatedButton.styleFrom(
+//                               backgroundColor: Colors.redAccent,
+//                             ),
+//                             child: Text(
+//                               'Continue',
+//                               style: TextStyle(color: Colors.white),
+//                             ),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   )
+//
+//                 ],
+//               ),
+//             ),
+//           ),
+//           backgroundColor: Colors.grey.shade100,
+//         );
+//       },
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:newconnection_buildingpemitdetails/responsive_row_column.dart';
 
@@ -11,7 +212,7 @@ void main() {
 }
 
 class BuildingPermitDetailsPage extends StatelessWidget {
-  const BuildingPermitDetailsPage({Key? key}) : super(key: key);
+  const BuildingPermitDetailsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -88,34 +289,51 @@ class BuildingPermitDetailsPage extends StatelessWidget {
                           Text("Customer Class *"),
                           SizedBox(
                             width: 450,
-                            child: TextField(
+                            child: DropdownButtonFormField<String>(
                               decoration: InputDecoration(
-                                hintText: 'Select Customer Class',
                                 border: OutlineInputBorder(),
                               ),
+                              hint: Text('Select Customer Class'),
+                              items: ['Residential', 'Commercial', 'Industrial'].map((value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                              onChanged: (value) {
+                                print("Selected Customer Class: $value");
+                              },
                             ),
                           ),
                         ],
                       ),
-                      // SizedBox(height: 1),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text("Premise Type *"),
                           SizedBox(
                             width: 450,
-                            child: TextField(
+                            child: DropdownButtonFormField<String>(
                               decoration: InputDecoration(
-                                hintText: 'Select Premise Type',
                                 border: OutlineInputBorder(),
                               ),
+                              hint: Text('Select Premise Type'),
+                              items: ['Apartment', 'Villa', 'Plot'].map((value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                              onChanged: (value) {
+                                print("Selected Premise Type: $value");
+                              },
                             ),
                           ),
                         ],
                       ),
                     ],
                   ),
-                  SizedBox(height: 16,),
+                  SizedBox(height: 16),
                   ResponsiveRowColumn(
                     spacing: 10,
                     children: [
@@ -125,18 +343,24 @@ class BuildingPermitDetailsPage extends StatelessWidget {
                           Text("Service Type"),
                           SizedBox(
                             width: 450,
-                            child: TextField(
+                            child: DropdownButtonFormField<String>(
                               decoration: InputDecoration(
-                                hintText: 'Select Service Type',
                                 border: OutlineInputBorder(),
                               ),
+                              hint: Text('Select Service Type'),
+                              items: ['New Connection', 'Upgrade', 'Temporary'].map((value) {
+                                return DropdownMenuItem<String>(
+                                  value: value,
+                                  child: Text(value),
+                                );
+                              }).toList(),
+                              onChanged: (value) {
+                                print("Selected Service Type: $value");
+                              },
                             ),
                           ),
                         ],
                       ),
-                      // SizedBox(height: 1),
-                      // Container()
-                      // SizedBox(width: 250), // fixed Expanded issue
                     ],
                   ),
 
